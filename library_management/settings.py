0511bd58b87fccd0fd7a3b7045c5abdc027bb26c
fahad <shaikfahad6305@gmail.com>
2025-11-22 13:47:20 +0530

    assuming localhost link which render gave

--- library_management/settings.py
+++ library_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6aka267=p_@1fk+@od)6b0+ii99n((%ekk9-^ru8bzu%x#cm^3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["library-management-9lf1.onrender.com","localhost","127.0.0.1"]
 
 
 # Application definition
